7e60a9ee22c76d40c3687348ebcba39522352325
hacker65536 <s.hacker65536@gmail.com>
2019-06-20 07:23:37 +0000

    Update

--- ope.sh
+++ ope.sh
@@ -5,10 +5,10 @@
 dir="./tf/ec2/"
 
 case $1 in
-	replace) 
+	restart) 
 		(cd $dir && terraform destroy -auto-approve && terraform apply -auto-approve && terraform output -json ips | jq -r 'to_entries[]| select(.key|contains("amz")) .user|="ec2-user" | select(.key|contains("cent")) .user|="centos" | select(.key|contains("ubuntu")) .user|="ubuntu" | "[\(.key)]",.key[8:]+" ansible_host="+.value+" ansible_user=" + .user' > ../../inventory)
 		;;
-	remove )
+	rm )
 		(cd $dir && terraform destroy -auto-approve )
 		;;
 	start )

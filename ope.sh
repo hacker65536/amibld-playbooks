ee18afcbc18a4516d5a8a8d4b124821fa63b4c0b
hacker65536 <s.hacker65536@gmail.com>
2019-06-20 08:32:46 +0000

    Update

--- ope.sh
+++ ope.sh
@@ -6,18 +6,18 @@ dir="./tf/ec2/"
 
 case $1 in
 	restart) 
-		(cd $dir && terraform destroy -auto-approve && terraform apply -auto-approve && terraform output -json ips | jq -r 'to_entries[]| select(.key|contains("amz")) .user|="ec2-user" | select(.key|contains("cent")) .user|="centos" | select(.key|contains("ubuntu")) .user|="ubuntu" | "[\(.key)]",.key[8:]+" ansible_host="+.value+" ansible_user=" + .user' > ../../inventory)
+		(cd $dir && terraform destroy -auto-approve && terraform apply -auto-approve && terraform output -json ips | jq -r 'to_entries[]| select(.key|contains("amz")) .user|="ec2-user" | select(.key|contains("cent")) .user|="centos" | select(.key|contains("ubuntu")) .user|="ubuntu" | .key[8:]+" ansible_host="+.value+" ansible_user=" + .user' > ../../inventory)
 		;;
-	rm )
+	rm | stop)
 		(cd $dir && terraform destroy -auto-approve )
 		;;
 	start )
-		(cd $dir && terraform apply -auto-approve && terraform output -json ips | jq -r 'to_entries[]| select(.key|contains("amz")) .user|="ec2-user" | select(.key|contains("cent")) .user|="centos" | select(.key|contains("ubuntu")) .user|="ubuntu" | "[\(.key)]",.key[8:]+" ansible_host="+.value+" ansible_user=" + .user' > ../../inventory)
+		(cd $dir && terraform apply -auto-approve && terraform output -json ips | jq -r 'to_entries[]| select(.key|contains("amz")) .user|="ec2-user" | select(.key|contains("cent")) .user|="centos" | select(.key|contains("ubuntu")) .user|="ubuntu" | .key[8:]+" ansible_host="+.value+" ansible_user=" + .user' > ../../inventory)
 		;;
 	list)
 		(cd $dir && terraform state list)
 		;;
 	init)
-		(cd $dir && terraform init)
+		(cd $dir && terraform init && terraform workspace new amitest)
 		;;
 esac
